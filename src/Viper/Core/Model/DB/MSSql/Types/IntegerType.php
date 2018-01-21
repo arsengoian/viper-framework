0961981e84adef16c112125a477a39367d9e2283
arsengoian <mirellastitan@gmail.com>
2018-01-21 20:04:35 +0200

    MSSQL TODOs

--- src/Viper/Core/Model/DB/MSSql/Types/IntegerType.php
+++ src/Viper/Core/Model/DB/MSSql/Types/IntegerType.php
@@ -8,7 +8,36 @@
 
 namespace Viper\Core\Model\DB\MSSql\Types;
 
-// TODO Support UNSIGNED
+// TODO Support all these:
+//    bit	Integer that can be 0, 1, or NULL
+//    tinyint	Allows whole numbers from 0 to 255	1 byte
+//    smallint	Allows whole numbers between -32,768 and 32,767	2 bytes
+//    int	Allows whole numbers between -2,147,483,648 and 2,147,483,647	4 bytes
+//    bigint	Allows whole numbers between -9,223,372,036,854,775,808 and 9,223,372,036,854,775,807	8 bytes
+//    decimal(p,s)	Fixed precision and scale numbers.
+//    Allows numbers from -10^38 +1 to 10^38 –1.
+//
+//    The p parameter indicates the maximum total number of digits that can be stored (both to the left and to the right of the decimal point). p must be a value from 1 to 38. Default is 18.
+//
+//    The s parameter indicates the maximum number of digits stored to the right of the decimal point. s must be a value from 0 to p. Default value is 0
+//
+//    5-17 bytes
+//    numeric(p,s)	Fixed precision and scale numbers.
+//    Allows numbers from -10^38 +1 to 10^38 –1.
+//
+//    The p parameter indicates the maximum total number of digits that can be stored (both to the left and to the right of the decimal point). p must be a value from 1 to 38. Default is 18.
+//
+//    The s parameter indicates the maximum number of digits stored to the right of the decimal point. s must be a value from 0 to p. Default value is 0
+//
+//    5-17 bytes
+//    smallmoney	Monetary data from -214,748.3648 to 214,748.3647	4 bytes
+//    money	Monetary data from -922,337,203,685,477.5808 to 922,337,203,685,477.5807	8 bytes
+//    float(n)	Floating precision number data from -1.79E + 308 to 1.79E + 308.
+//    The n parameter indicates whether the field should hold 4 or 8 bytes. float(24) holds a 4-byte field and float(53) holds an 8-byte field. Default value of n is 53.
+//
+//    4 or 8 bytes
+//    real	Floating precision number data from -3.40E + 38 to 3.40E + 38	4 bytes
+
 
 use Viper\Core\Model\DB\Types\SizedType;
 
--- src/Viper/Core/Model/DB/MSSql/Types/StringType.php
+++ src/Viper/Core/Model/DB/MSSql/Types/StringType.php
@@ -12,14 +12,27 @@ namespace Viper\Core\Model\DB\MSSql\Types;
 use Viper\Core\Model\DB\Types\SizedType;
 use Viper\Core\Routing\HttpException;
 use Viper\Support\ValidationException;
+// TODO support:
+//    char(n)	Fixed width character string	8,000 characters	Defined width
+//    varchar(n)	Variable width character string	8,000 characters	2 bytes + number of chars
+//    varchar(max)	Variable width character string	1,073,741,824 characters	2 bytes + number of chars
+//    text	Variable width character string	2GB of text data	4 bytes + number of chars
+//    nchar	Fixed width Unicode string	4,000 characters	Defined width x 2
+//    nvarchar	Variable width Unicode string	4,000 characters
+//    nvarchar(max)	Variable width Unicode string	536,870,912 characters
+//    ntext	Variable width Unicode string	2GB of text data
+//    binary(n)	Fixed width binary string	8,000 bytes
+//    varbinary	Variable width binary string	8,000 bytes
+//    varbinary(max)	Variable width binary string	2GB
+//    image	Variable width binary string	2GB
 
 // TODO support VARCHAR(max)
 class StringType extends SizedType
 {
 
     const TYPES = [
-        'CHAR' => 255,
-        'VARCHAR' => 255,
+        'CHAR' => 8000,
+        'VARCHAR' => 8000,
         'TINYTEXT' => 255,
         'TEXT' => 65535,
         'BLOB' => 65535,
